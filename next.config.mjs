713d0244f0550f4a2e4f271f924045f430caf407
Cuong Nguyen Huu <cuongnh@fint.vn>
2025-11-13 23:02:01 +0700

    fix: remove unused package  in the codebase

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
-import { fileURLToPath } from 'node:url';
-
 import withBundleAnalyzer from '@next/bundle-analyzer';
-import createJiti from 'jiti';
 import withNextIntl from 'next-intl/plugin';
-
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-jiti('./src/libs/Env');
 
 const withNextIntlConfig = withNextIntl('./src/libs/i18n.ts');
 
